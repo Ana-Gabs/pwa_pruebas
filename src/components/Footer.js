642466ee5fcde00e4ac843f9361b39453f1de785
Ana-Gabs <2022371069@uteq.edu.mx>
2025-10-20 04:00:18 -0600

    cambio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer style={{ background: '#f5f5f5', padding: '1rem', textAlign: 'center' }}>
-            <small>© {new Date().getFullYear()} - Mi PWA - Manuel Contreras Castillo</small>
+            <small>© {new Date().getFullYear()} - PWA - IDGS11 - Manuel Contreras Castillo</small>
         </footer>
     );
 }
